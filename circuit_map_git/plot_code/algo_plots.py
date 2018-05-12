e385c0892c8993a02f9d1c48fdc126eeeb352fe9
yvinkesteijn <yevinkesteijn@gmail.com>
2018-05-12 14:18:05 +0200

    hc plot

--- circuit_map_git/plot_code/algo_plots.py
+++ circuit_map_git/plot_code/algo_plots.py
@@ -6,9 +6,9 @@ class algo_plots:
     def __init__(self):
         self.hc_dir = 'HC/'
         self.ppa_dir = 'PPA/'
-        # self.hc_dct = self.hc_data()
-        self.ppa_dct = self.ppa_data()
-        # self.hc_plot()
+        self.hc_dct = self.hc_data()
+        # self.ppa_dct = self.ppa_data()
+        self.hc_plot()
 
 
     def hc_data(self):
@@ -24,11 +24,16 @@ class algo_plots:
             with open(self.hc_dir+file) as csvfile:
                 csv_rd = csv.reader(csvfile)
                 print(file)
+                h_val = 0.0000
                 for idx,row in enumerate(csv_rd):
+                    print(row)
+                    break
                     cur_con, cur_len = int(row[0]), int(row[1][0:4])
                     if cur_con > high_connect or (cur_con == high_connect and cur_len < high_len):
                         high_connect, high_len = copy.copy(cur_con), copy.copy(cur_len)
                         h_val = row[0]+'.'+row[1][0:4]
+                    else:
+                        h_val
                         it_lst.append(idx)
                         score_lst.append(float(h_val))
                 result_dict[i]['iter'], result_dict[i]['score'] = it_lst, score_lst
@@ -56,7 +61,7 @@ class algo_plots:
             with open(self.ppa_dir+file) as csvfile:
                 csv_rd = csv.reader(csvfile)
                 result_dct[i]['iter'], result_dct[i]['score'] = it_lst, score_lst
-                gen_count, gen_lst = 1, [], []
+                gen_count, gen_lst,iter_lst = 1, [], []
                 for j, row in enumerate(csv_rd):
                     if row[0].startswith('##'):
                         ppa_dct[ppa_name][gen_count] = gen_lst
