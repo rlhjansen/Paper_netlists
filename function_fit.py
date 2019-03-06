0a88f039a246db16dd93cb312c91ad3ed49f25f6
rlhjansen <rlh.jansen@outlook.com>
2019-03-06 12:52:14 +0100

    function fit

--- function_fit.py
+++ function_fit.py
@@ -20,31 +20,31 @@ print(df[['netlist length', rand_solv_str(20), best_solv_str(1, 20)]])
 # print(df.head())
 
 
-def func(c):
-    def expfunc(n, const1, const2):
-        return np.exp(-np.exp(2*n*const1)*c*const2)
-    return expfunc
+def expfunc(nl, const1, const2):
+    return np.exp(-np.exp(2*const1*nl)*const2)
 
 
 
 for cs in [20,30,40,50,60,70,80,90,100]:
     nl = df['netlist length']
     y_rand = df[rand_solv_str(cs)]
-    expfunc = func(cs)
     plt.plot(nl, y_rand, 'b-', label='data')
     popt, pcov = curve_fit(expfunc, nl, y_rand, bounds=(0, [0.08, 0.1]))
     plt.plot(nl, expfunc(nl, *popt), 'g--', label='fit: const1=%5.3f, const2=%5.8f' % tuple(popt))
     plt.xlabel('x')
     plt.ylabel('y')
     plt.legend()
-    plt.savefig("fitted_rand_" + str(chipsize) + ".png")
+    plt.savefig("fitted_rand_" + str(cs) + ".png")
+    plt.show()
+    plt.clf()
 
     nl = df['netlist length']
-    y_rand = df[best_solv_str(200, cs)]
+    y_rand = df[best_solv_str(1, cs)]
     plt.plot(nl, y_rand, 'b-', label='data')
     popt, pcov = curve_fit(expfunc, nl, y_rand, bounds=(0, [0.08, 0.1]))
     plt.plot(nl, expfunc(nl, *popt), 'g--', label='fit: const1=%5.3f, const2=%5.8f' % tuple(popt))
     plt.xlabel('x')
     plt.ylabel('y')
     plt.legend()
-    plt.savefig("fitted_rand_" + str(chipsize) + ".png")
+    plt.savefig("fitted_rand_" + str(cs) + ".png")
+    plt.clf()
